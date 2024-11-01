bd0f2e5b072f7d4e52096bdc3bfab7a5a5ccd781
YeBaited <jamespueblas@gmail.com>
2024-11-01 15:01:30 +0800

    Made the Home control panel beautiful

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import CreateList from "./Components/createList"
+import CreateList from "./Components/CreateList"
+import { useNavigate } from "react-router-dom"
 
 
 function Home(){
+    const nv = useNavigate()
     const [CurrentData, SetData] = useState([])
 
     function getData(){
@@ -24,9 +26,22 @@ function Home(){
     
     return (
         <>
-            <div>
-                <button onClick={getData}>Refresh</button>
-                {CurrentData.map(d => <CreateList Bid={d[0]} Busr={d[1]} Bpas={d[2]}   />)}
+            <div className="bg-red-300">
+                <h1 className="font-bold">List of DB</h1>
+                <div className="bg-slate-500 min-h-[10rem]">
+                    {CurrentData.map(d => <CreateList key={d[0]} Bid={d[0]} Busr={d[1]} Bpas={d[2]}   />)}
+
+                </div>
+                <div className="flex flex-col sm:flex-row py-1">
+                    <button className="mx-2 px-2 border border-black" onClick={getData}>Refresh List</button>
+                    <input type="text" className="mx-2 border border-black" placeholder="Enter id"/>
+                    <button className="mx-2 px-2 border border-black">Delete</button>
+                    <input type="text" className="mx-2 border border-black" placeholder="Search name"/>
+                    <button className="mx-2 px-2 border border-black">Search</button>
+                    <input type="text" className="mx-2 border border-black" placeholder="Custom Query"/>
+                    <button className="mx-2 px-2 border border-black">Execute</button>
+                    <button className="mx-2 px-2 border border-black">Go to login</button>
+                </div>
             </div>
         </>
     )
