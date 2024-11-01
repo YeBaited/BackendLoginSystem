309d220637d7b17c6620516ad255886a13caad0a
YeBaited <jamespueblas@gmail.com>
2024-11-01 13:33:39 +0800

    Created the CreateList Component

---
+++ src/Components/CreateList.tsx
@@ -0,0 +1,20 @@
+interface Requirement {
+    Bid: string,
+    Busr: string,
+    Bpas: string,
+}
+
+function CreateList({Bid, Busr, Bpas} : Requirement){
+    return (
+        <>
+            <div className="bg-red-300 flex flex-row">
+                <h1 className="mx-2"> <span className="font-bold">Id:</ span> {Bid}</h1>
+                <h1 className="mx-2"> <span className="font-bold">Username:</ span> {Busr}</h1>
+                <h1 className="mx-2"> <span className="font-bold">Password:</ span> {Bpas}</h1>
+                
+            </div>
+        </>
+    )
+}
+
+export default CreateList
