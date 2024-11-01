900be7e012a5637b4132e4d1892254c93849fee0
YeBaited <jamespueblas@gmail.com>
2024-11-01 15:01:54 +0800

    Just added some border to the list

--- src/Components/CreateList.tsx
+++ src/Components/CreateList.tsx
@@ -7,7 +7,7 @@ interface Requirement {
 function CreateList({Bid, Busr, Bpas} : Requirement){
     return (
         <>
-            <div className="bg-red-300 flex flex-row">
+            <div className="border border-red-300 flex flex-row">
                 <h1 className="mx-2"> <span className="font-bold">Id:</ span> {Bid}</h1>
                 <h1 className="mx-2"> <span className="font-bold">Username:</ span> {Busr}</h1>
                 <h1 className="mx-2"> <span className="font-bold">Password:</ span> {Bpas}</h1>
