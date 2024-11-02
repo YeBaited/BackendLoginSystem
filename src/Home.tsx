e9150c073670644bcd9d7619598b6f71aa00fd45
YeBaited <jamespueblas@gmail.com>
2024-11-02 12:20:11 +0800

    idk

--- src/Home.tsx
+++ src/Home.tsx
@@ -19,20 +19,57 @@ function Home(){
     
     }
 
-    console.log(CurrentData)
+    function ajaxRequest(dataToPass : string, fga : Function ){
+
+        var xhtml = new XMLHttpRequest()
+
+        xhtml.onreadystatechange = function(){
+            if (this.readyState == 4 || this.readyState == 400){
+                fga()
+            }
+        }
+
+        xhtml.open("POST", "http://localhost:82/php/HomePanelHandling.php")
+        xhtml.send(dataToPass)
+    }
+
+
+    function Delete(){
+        const numInput = document.querySelector("#deleteSelector") as HTMLInputElement
+
+        if (!numInput.value) return
+
+        const data = {
+            "query" : `DELETE FROM logindb.logincredentials WHERE id =" ${numInput}`
+        }
+
+        ajaxRequest(JSON.stringify(data), ()=>{
+            alert("Sucessfully deleted ID:" + numInput.value)
+        })
+
+        
+    }
+
+    function Search(){
+
+    }
+
+    function ExecuteQuery(){
+
+    }
     
     return (
         <>
             <div className="bg-red-300">
-                <h1 className="font-bold">List of DB</h1>
+                <h1 className="font-bold">List of data Stored!</h1>
                 <div className="bg-slate-500 min-h-[10rem]">
                     {CurrentData.map(d => <CreateList key={d[0]} Bid={d[0]} Busr={d[1]} Bpas={d[2]}   />)}
 
                 </div>
                 <div className="flex flex-col sm:flex-row py-1">
                     <button className="mx-2 px-2 border border-black" onClick={getData}>Refresh List</button>
-                    <input type="text" className="mx-2 border border-black" placeholder="Enter id"/>
-                    <button className="mx-2 px-2 border border-black">Delete</button>
+                    <input type="number" className="mx-2 border border-black" placeholder="Enter id" id="deleteSelector"/>
+                    <button className="mx-2 px-2 border border-black" onClick={Delete}>Delete</button>
                     <input type="text" className="mx-2 border border-black" placeholder="Search name"/>
                     <button className="mx-2 px-2 border border-black">Search</button>
                     <input type="text" className="mx-2 border border-black" placeholder="Custom Query"/>
